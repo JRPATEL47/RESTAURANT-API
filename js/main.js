3591d8301d0fc9d590bc65b77d29d76de5d388b5
Jenish Patel <84110159+JRPATEL47@users.noreply.github.com>
2022-04-28 01:25:47 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,13 +1,6 @@
-/***********************************************************************************
- * WEB422 – Assignment 2
- * I declare that this assignment is my own work in accordance with Seneca Academic Policy.
- * No part of this assignment has been copied manually or electronically from any other source
- * (including web sites) or distributed to other students.
- *
- * Name: ____Jenish Rajnikant Patel_____ Student ID: __131733206____ Date: ___01-02-2022___
+/*
  * Heroku Link:https://secure-thicket-54269.herokuapp.com/
- *
- ********************************************************************************/
+*/
 
 var restaurantData = [];
 
